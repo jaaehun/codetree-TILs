9979a4f705153127e5c1a335a07b399956614c46
이재훈 <mpp11111@naver.com>
2024-09-15 17:42:24 +0900

    Title: 산타의 선물 공장, Time: 232ms, Memory: 13MB, Status: Passed - CodeTree

--- 240915/산타의 선물 공장/santa-gift-factory.cpp
+++ 240915/산타의 선물 공장/santa-gift-factory.cpp
@@ -1,352 +1,354 @@
-#include <iostream>
+#include <stdio.h>
 #include <unordered_map>
-#include <vector>
-
-#define MAX_BELT 15
-#define MAX_BOX 100010
 
 using namespace std;
 
-struct BELT {
-	int num;
-	int front;
-	int back;
-	int state;
-};
+typedef long long ll;
 
-struct BOX {
+#define MAX (200000 + 5000)
+
+#define FACTORY (100)
+#define UNLOAD (200)
+#define DELETE (300)
+#define CHECK (400)
+#define BROKEN (500)
+
+#define UNLOAD_BOX (600) // 이름 변경 
+#define BROKEN_BELT (700)
+#define EMPTY_BELT (-1)
+#define CHECK_DELETE (-2)
+
+int T;
+int Q, COMMAND, N, M;
+int ID[MAX];
+int W[MAX];
+
+typedef struct st1
+{
 	int weight;
-	int belt_id;
+	int unload;
+}BOXINFO;
+
+typedef struct st2
+{
+	int id;
+	int weight;
+	int unload;
 	int prev;
 	int next;
-};
+}BELT;
 
-int n, m;
-int input[MAX_BOX];
-unordered_map<int, BOX> box;
-unordered_map<int, BELT> belt;
+BOXINFO BOX[MAX];
+BELT HEAD[10 + 5];
+BELT TAIL[10 + 5];
+BELT NODE[MAX];
+int nIdx;
 
-void FUNC(int Q)
+int checkBelt[10 + 5];
+
+unordered_map<int, int> boxMap;
+unordered_map<int, int> beltMap;
+
+void printBelt(int beltNum)
 {
-	if (Q == 100)
+	int index = HEAD[beltNum].next;
+
+	printf("belt %d : ", beltNum);
+	while (1)
 	{
-		cin >> n >> m;
+		if (index == -1) break;
 
-		int cnt = 0;
-		int belt_id = 1;
-		int pivot = n/m;
-		for (int i = 0; i < n; i++)
-		{
-			int id;
-			cin >> id;
-			// BOX ID의 input 배열
-			input[i] = id;
-			// belt 해쉬맵 만들기
-			if (cnt == 0)
-			{
-				belt[belt_id].front = id;
-				belt[belt_id].back = id;
-				belt[belt_id].state = 0;
-				belt[belt_id].num = pivot;
+		BELT tmp = NODE[index];
 
-				box[id].belt_id = belt_id;
-				box[id].prev = -1;
-				box[id].next = -1;
-			}
-			else if (cnt == pivot - 1)
-			{
-				belt[belt_id].back = id;
-				
-				box[id].belt_id = belt_id;
-				box[id].prev = input[i - 1];
-				box[id].next = -1;
-				box[input[i - 1]].next = id;
-			}
-			else {
-				box[id].belt_id = belt_id;
-				box[id].prev = input[i - 1];
-				box[input[i - 1]].next = id;
-			}
-			cnt++;
-			if (cnt == pivot)
-			{
-				cnt = 0;
-				belt_id += 1;
-			}
-		}
-		for (int i = 0; i < n; i++)
-		{
-			int w = 0;
-			cin >> w;
-			box[input[i]].weight = w;
-		}
+		printf("(%d : %d), ", tmp.id, tmp.weight);
+
+		index = tmp.next;
 	}
-	else if (Q == 200)
+
+	printf("TAIL %d %d", NODE[TAIL[beltNum].prev].id, NODE[TAIL[beltNum].prev].weight);
+
+	putchar('\n');
+}
+
+void printAll()
+{
+	for (int m = 1; m <= M; m++)
 	{
-		int w_max;
-		cin >> w_max;
-		int cnt = 0;
-		// m개 벨트 탐색
-		for (int i = 1; i <= m; i++)
-		{
-			if (belt[i].num == 0)
-				continue;
-			
-			int id = belt[i].front;
-			// 하차
-			if (box[id].weight <= w_max)
-			{
-				cnt += box[id].weight;
-				// box가 1개인 경우
-				if (belt[i].num == 1)
-				{
-					belt[i].front = -1;
-					belt[i].back = -1;
-
-					box[id] = { 0, 0, -1, -1 };
-				}
-				// box가 여러개인 경우
-				else
-				{
-					int next = box[id].next;
-					box[id] = { 0, 0, -1, -1 };
-					box[next].prev = -1;
-
-					belt[i].front = next;
-				}
-				belt[i].num -= 1;
-			}
-			else
-			{
-				// box가 1개인 경우
-				if (belt[i].num == 1) continue;
-				// box가 여러개인 경우
-				int next = box[id].next;
-				int back = belt[i].back;
-
-				box[id].prev = back;
-				box[id].next = -1;
-
-				box[next].prev = -1;
-				box[back].next = id;
-
-				belt[i].front = next;
-				belt[i].back = id;
-			}
-		}
-		cout << cnt << endl;
+		if (checkBelt[m] == BROKEN_BELT) continue;
+		printBelt(m);
 	}
-	else if (Q == 300)
+	putchar('\n');
+}
+
+void changeBelt(int beltNum, int changeNum)
+{
+	int index = HEAD[beltNum].next;
+
+	while (1)
 	{
-		int r_id;
-		cin >> r_id;
-		// box가 있으면
-		if (box[r_id].weight != 0)
-		{
-			int belt_id = box[r_id].belt_id;
-			int prev = box[r_id].prev;
-			int next = box[r_id].next;
-			belt[belt_id].num -= 1;
+		if (index == -1) break;
 
-			// 벨트 위의 유일한 박스이면
-			if (prev == -1 && next == -1)
-			{
-				belt[belt_id].front = -1;
-				belt[belt_id].back = -1;
-			}
-			// 맨 앞이면
-			else if (prev == -1)
-			{
-				belt[belt_id].front = next;
-				box[next].prev = -1;
-			}
-			// 맨 뒤면
-			else if (next == -1)
-			{
-				belt[belt_id].back = prev;
-				box[prev].next = -1;
-			}
-			else
-			{
-				box[prev].next = next;
-				box[next].prev = prev;
-			}
-			box[r_id] = { 0, 0, -1, -1 };
-			cout << r_id << endl;
-		}
-		// box가 없으면
-		else 
-		{
-			box[r_id] = { 0, 0, -1, -1 };
-			cout << -1 << endl;
-		}
-	}
-	else if (Q == 400)
-	{
-		int f_id;
-		cin >> f_id;
-		// box가 없다면
-		if (box[f_id].weight == 0)
-		{
-			cout << -1 << endl;
-		}
-		// box가 있다면
-		else
-		{
-			int prev = box[f_id].prev;
-			int next = box[f_id].next;
-			int belt_id = box[f_id].belt_id;
-			int front = belt[belt_id].front;
-			int back = belt[belt_id].back;
-			
-			cout << belt_id << endl;
-			if (belt[belt_id].num == 1) return;
-			if (prev == -1) return ;
-			else if (next == -1)
-			{
-				belt[belt_id].front = f_id;
-				belt[belt_id].back = prev;
-
-				box[f_id].prev = -1;
-				box[prev].next = -1;
-
-				box[front].prev = f_id;
-				box[f_id].next = front;
-			}
-			else
-			{
-				belt[belt_id].front = f_id;
-				belt[belt_id].back = prev;
-
-				box[f_id].prev = -1;
-				box[prev].next = -1;
-
-				box[back].next = front;
-				box[front].prev = back;
-			}
-		}
-
-	}
-	else if (Q == 500)
-	{
-		int b_num;
-		cin >> b_num;
-		// 벨트가 이미 고장
-		if (belt[b_num].state == -1)
-		{
-			cout << -1 << endl;
-			return;
-		}
-		else cout << b_num << endl;
-		
-		// 고장 벨트에 박스가 0개
-		if (belt[b_num].num == 0)
-		{
-			// 사용할 수 없게 됨
-			belt[b_num] = { 0, -1, -1, -1 };
-			return;
-		}
-		// 정상 벨트 조회
-		int flag = 0;
-		for (int i = b_num + 1 ; i <= m; i++)
-		{
-			if (belt[i].state == -1) continue;
-			// 정상 벨트 찾음
-			flag = 1;
-			belt[i].num += belt[b_num].num;
-			// 정상 벨트에 박스가 0개
-			if (belt[i].num == 0)
-			{
-				belt[i].front = belt[b_num].front;
-				belt[i].back = belt[b_num].back;
-				// 박스이동
-				int k = belt[b_num].front;
-				while (box[k].next != -1)
-				{
-					box[k].belt_id = i;
-					k = box[k].next;
-				}
-				box[k].belt_id = i;
-			}
-			else {
-				int from_front = belt[b_num].front;
-				int to_back = belt[i].back;
-				
-				box[to_back].next = from_front;
-				box[from_front].prev = to_back;
-				
-				belt[i].back = belt[b_num].back;
-				// 박스이동
-				while (box[from_front].next != -1)
-				{
-					box[from_front].belt_id = i;
-					from_front = box[from_front].next;
-				}
-				box[from_front].belt_id = i;
-				belt[i].back = from_front;
-			}
-			// belt[i].num += belt[b_num].num;
-			// 사용할 수 없게 됨
-			belt[b_num] = { 0, -1, -1, -1 };
-			if (flag != 0) break;
-		}
-		// 못찾은 경우 1번부터 다시 조회
-		if (flag == 0)
-		{
-			for (int i = 1; i < b_num; i++)
-			{
-				if (belt[i].state == -1) continue;
-				flag = 1;
-				// 정상 벨트에 박스가 0개
-				if (belt[i].num == 0)
-				{
-					belt[i].front = belt[b_num].front;
-					belt[i].back = belt[b_num].back;
-					// 박스이동
-					int k = belt[b_num].front;
-					while (box[k].next != -1)
-					{
-						box[k].belt_id = i;
-						k = box[k].next;
-					}
-					box[k].belt_id = i;
-				}
-				else {
-					int from_front = belt[b_num].front;
-					int to_back = belt[i].back;
-
-					box[to_back].next = from_front;
-					box[from_front].prev = to_back;
-
-					belt[i].back = belt[b_num].back;
-					// 박스이동
-					while (box[from_front].next != -1)
-					{
-						box[from_front].belt_id = i;
-						from_front = box[from_front].next;
-					}
-					box[from_front].belt_id = i;
-					belt[i].back = from_front;
-				}
-				belt[i].num += belt[b_num].num;
-				// 사용할 수 없게 됨
-				belt[b_num] = { 0, -1, -1, -1 };
-				if (flag != 0) break;
-			}
-		}
-		
+		BELT tmp = NODE[index];
+		beltMap[tmp.id] = changeNum;
+		index = tmp.next;
 	}
 }
 
-int main()
+void input()
 {
-	int Q;
-	cin >> Q;
-	for (int i = 0; i < Q; i++)
+	scanf("%d %d", &N, &M);
+
+	boxMap.clear();
+	beltMap.clear();
+
+	for (int m = 1; m <= M; m++) checkBelt[m] = 0;
+
+	for (int n = 0; n < N; n++) scanf("%d", &ID[n]);
+	for (int n = 0; n < N; n++) scanf("%d", &W[n]);
+
+	for (int n = 0; n < N; n++)
 	{
-		int order;
-		cin >> order;
-		FUNC(order);
+		boxMap[ID[n]] = n;
+		BOX[n].unload = 0;
+		BOX[n].weight = W[n];
+	}
+
+	nIdx = 0;
+
+	int	index = 0;
+	int count = N / M;
+	for (int m = 1; m <= M; m++)
+	{
+		HEAD[m].prev = -1;
+		TAIL[m].next = -1;
+
+		int startIndex = index;
+		for (int i = 0; i < count; i++)
+		{
+			NODE[nIdx].id = ID[index];
+			NODE[nIdx].weight = W[index];
+			NODE[nIdx].unload = 0;
+			NODE[nIdx].prev = index - 1;
+			NODE[nIdx++].next = index + 1;
+
+			beltMap[ID[index++]] = m; // box의 belt 위치
+		}
+
+		NODE[startIndex].prev = -1;
+		NODE[nIdx - 1].next = -1; // 마지막 node는 Tail로 설정
+		HEAD[m].next = startIndex;
+		TAIL[m].prev = index - 1;
+	}
+}
+
+void unload(int w_max)
+{
+	ll totalWeight = 0;
+	for (int m = 1; m <= M; m++)
+	{
+		int firstBoxIndex = HEAD[m].next;
+		if (firstBoxIndex == -1 || checkBelt[m] == BROKEN_BELT) continue;
+
+		int frontNextIndex = NODE[firstBoxIndex].next;
+
+		HEAD[m].next = frontNextIndex;
+		if (frontNextIndex == -1) TAIL[m].prev = -1;
+		else NODE[frontNextIndex].prev = -1;
+
+		int weight = NODE[firstBoxIndex].weight;
+		if (weight <= w_max)
+		{
+			NODE[firstBoxIndex].unload = UNLOAD;
+			totalWeight += weight;
+		}
+		else
+		{
+			int lastBoxIndex = TAIL[m].prev;
+			if (lastBoxIndex == -1) // 상자가 1개인 경우.
+			{
+				HEAD[m].next = firstBoxIndex;
+				NODE[firstBoxIndex].prev = -1;
+				NODE[firstBoxIndex].next = -1;
+				TAIL[m].prev = firstBoxIndex;
+			}
+			else
+			{
+				NODE[lastBoxIndex].next = firstBoxIndex;
+				NODE[firstBoxIndex].prev = lastBoxIndex;
+				NODE[firstBoxIndex].next = -1;
+				TAIL[m].prev = firstBoxIndex;
+			}
+		}
+	}
+
+	printf("%lld\n", totalWeight);
+}
+
+void deleteBox(int r_id)
+{
+	if (boxMap.count(r_id) == 0)
+	{
+		printf("%d\n", -1);
+		return;
+	}
+
+	int boxIndex = boxMap[r_id];
+	if (NODE[boxIndex].unload == UNLOAD)
+	{
+		printf("%d\n", -1);
+		return;
+	}
+
+	int beltNum = beltMap[r_id];
+
+	int prevBoxIndex = NODE[boxIndex].prev;
+	int nextBoxIndex = NODE[boxIndex].next;
+
+	if (prevBoxIndex != -1) NODE[prevBoxIndex].next = nextBoxIndex;
+	else HEAD[beltNum].next = nextBoxIndex;
+
+	if (nextBoxIndex != -1) NODE[nextBoxIndex].prev = prevBoxIndex;
+	else TAIL[beltNum].prev = prevBoxIndex;
+
+	NODE[boxIndex].unload = UNLOAD;
+
+	printf("%d\n", r_id);
+}
+
+void checkBox(int f_id)
+{
+	if (boxMap.count(f_id) == 0)
+	{
+		printf("%d\n", -1);
+		return;
+	}
+
+	int boxIndex = boxMap[f_id];
+	if (NODE[boxIndex].unload == UNLOAD)
+	{
+		printf("%d\n", -1);
+		return;
+	}
+
+	int beltNum = beltMap[f_id];
+	int prevBoxIndex = NODE[boxIndex].prev;  // 선택된 상자의 바로 앞 상자
+	if (prevBoxIndex == -1) // 가장 앞에 있는 경우는 옮길 필요가 없다.
+	{
+		printf("%d\n", beltNum);
+		return;
+	}
+
+	int firstBoxIndex = HEAD[beltNum].next; // 가장 앞의 상자
+	int lastBoxIndex = TAIL[beltNum].prev; // 가장 뒤의 상자
+
+	HEAD[beltNum].next = boxIndex;
+	NODE[boxIndex].prev = -1;
+
+	NODE[firstBoxIndex].prev = lastBoxIndex;
+	NODE[prevBoxIndex].next = -1;
+
+	NODE[lastBoxIndex].next = firstBoxIndex;
+	TAIL[beltNum].prev = prevBoxIndex;
+
+	printf("%d\n", beltNum);
+}
+
+void brokenBelt(int b_num)
+{
+	if (checkBelt[b_num] == BROKEN_BELT)
+	{
+		printf("%d\n", -1);
+		return;
+	}
+
+	checkBelt[b_num] = BROKEN_BELT;
+
+	int nextBeltNum = -1;
+	for (int m = b_num + 1; m <= M; m++)
+	{
+		if (checkBelt[m] == BROKEN_BELT) continue;
+		else
+		{
+			nextBeltNum = m;
+			break;
+		}
+	}
+
+	if (nextBeltNum == -1) // 고장난 벨트 오른쪽에 정상 벨트가 없는 경우
+	{
+		for (int m = 1; m < b_num; m++)
+		{
+			if (checkBelt[m] == BROKEN_BELT) continue;
+			else
+			{
+				nextBeltNum = m;
+				break;
+			}
+		}
+	}
+
+	int brokenFirstBoxIndex = HEAD[b_num].next; // 고장난 벨트의 앞 상자
+	int brokenLastBoxIndex = TAIL[b_num].prev; // 고장난 벨트의 뒤 상자
+	int lastBoxIndex = TAIL[nextBeltNum].prev; // 옮길 벨트의 뒤 상자
+
+	if (brokenFirstBoxIndex == -1) // 고장난 벨트가 빈 경우
+	{
+		printf("%d\n", b_num);
+		return;
+	}
+
+	// 벨트 변경
+	changeBelt(b_num, nextBeltNum);
+
+	if (lastBoxIndex == -1) // 옮길 벨트가 빈 경우
+	{
+		HEAD[nextBeltNum].next = brokenFirstBoxIndex;
+		TAIL[nextBeltNum].prev = brokenLastBoxIndex;
+	}
+	else
+	{
+		NODE[lastBoxIndex].next = brokenFirstBoxIndex;
+		NODE[brokenFirstBoxIndex].prev = lastBoxIndex;
+
+		NODE[brokenLastBoxIndex].next = -1;
+		TAIL[nextBeltNum].prev = brokenLastBoxIndex;
+	}
+
+	printf("%d\n", b_num);
+}
+
+int main(void)
+{
+	scanf("%d", &Q);
+	for (int q = 0; q < Q; q++)
+	{
+		scanf("%d", &COMMAND);
+
+		if (COMMAND == FACTORY) input();
+		else if (COMMAND == UNLOAD)
+		{
+			int w_max;
+			scanf("%d", &w_max);
+			unload(w_max);
+		}
+		else if (COMMAND == DELETE)
+		{
+			int r_id;
+			scanf("%d", &r_id);
+			deleteBox(r_id);
+		}
+		else if (COMMAND == CHECK)
+		{
+			int f_id;
+			scanf("%d", &f_id);
+			checkBox(f_id);
+		}
+		else if (COMMAND == BROKEN)
+		{
+			int b_num;
+			scanf("%d", &b_num);
+			brokenBelt(b_num);
+		}
 	}
 
 	return 0;
