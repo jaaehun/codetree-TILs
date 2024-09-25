e137d1d6dddedfe491e745869d477620c9592fd7
이재훈 <mpp11111@naver.com>
2024-09-25 22:03:59 +0900

    Title: 나무박멸, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

--- 240925/나무박멸/tree-kill-all.cpp
+++ 240925/나무박멸/tree-kill-all.cpp
@@ -3,200 +3,179 @@
 
 using namespace std;
 
-int n, k, c;
+int N, K, C;
 int map[21][21];
-int copy_map[21][21];
-int destroy_map[21][21];
-int sum;
-int flag;
+int tmp_map[21][21];
+int death_map[21][21];
+int kill_map[21][21];
+int result;
 
-int dy[4] = {0,0,-1,1};
-int dx[4] = {-1,1,0,0};
-int dyy[4] = {-1,-1,1,1};
-int dxx[4] = {-1,1,-1,1};
+int dy[4] = { -1,1,0,0 };
+int dx[4] = { 0,0,-1,1 };
+int dyy[4] = { -1,-1,1,1 };
+int dxx[4] = { -1,1,-1,1 };
 
-void grow_tree() {
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(map[i][j] > 0){
-                int cnt = 0;
-                for(int d=0;d<4;d++){
-                    int now_y = i + dy[d];
-                    int now_x = j + dx[d];
+void grow() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (map[i][j] > 0) {
+				int cnt = 0;
+				for (int d = 0; d < 4; d++) {
+					int now_y = i + dy[d];
+					int now_x = j + dx[d];
 
-                    if(now_y >= 0 && now_y < n && now_x >= 0 && now_x < n){
-                        if(map[now_y][now_x] > 0){
-                            cnt++;
-                        }
-                    }
-                }
-                map[i][j] += cnt;
-            }
-        }
-    }
+					if (now_y >= 0 && now_y < N && now_x >= 0 && now_x < N) {
+						if (map[now_y][now_x] > 0) {
+							cnt++;
+						}
+					}
+				}
+				map[i][j] += cnt;
+			}
+		}
+	}
 }
 
-void spread_tree() {
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(map[i][j] > 0){
-                int cnt = 0;
-                int tree = map[i][j];
-                for(int d=0;d<4;d++){
-                    int now_y = i + dy[d];
-                    int now_x = j + dx[d];
+void breed() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (map[i][j] > 0) {
+				int cnt = 0;
+				for (int d = 0; d < 4; d++) {
+					int now_y = i + dy[d];
+					int now_x = j + dx[d];
 
-                    if(now_y >= 0 && now_y < n && now_x >= 0 && now_x < n){
-                        if(map[now_y][now_x] == 0){
-                            cnt++;
-                        }
-                    }
-                }
-                if(cnt > 0){
-                    int new_tree = tree / cnt;
-                    for(int d=0;d<4;d++){
-                        int now_y = i + dy[d];
-                        int now_x = j + dx[d];
+					if (now_y >= 0 && now_y < N && now_x >= 0 && now_x < N) {
+						if (map[now_y][now_x] == 0 && death_map[now_y][now_x] == 0) {
+							cnt++;
+						}
+					}
+				}
+				if (cnt != 0) {
+					int num = map[i][j] / cnt;
+					for (int d = 0; d < 4; d++) {
+						int now_y = i + dy[d];
+						int now_x = j + dx[d];
 
-                        if(now_y >= 0 && now_y < n && now_x >= 0 && now_x < n){
-                            if(map[now_y][now_x] == 0){
-                                copy_map[now_y][now_x] += new_tree;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(copy_map[i][j] > 0){
-                map[i][j] = copy_map[i][j];
-            }
-        }
-    }
-    memset(copy_map,0,sizeof(copy_map));
+						if (now_y >= 0 && now_y < N && now_x >= 0 && now_x < N) {
+							if (map[now_y][now_x] == 0 && death_map[now_y][now_x] == 0) {
+								tmp_map[now_y][now_x] += num;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (tmp_map[i][j] > 0) {
+				map[i][j] = tmp_map[i][j];
+			}
+		}
+	}
+
+	memset(tmp_map, 0, sizeof(tmp_map));
 }
 
-void destroy_tree() {
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(map[i][j] > 0){
-                int cnt = 0;
-                cnt += map[i][j];
-                for(int d=0;d<4;d++){
-                    for(int s=1;s<=k;s++){
-                        int now_y = i + s*dyy[d];
-                        int now_x = j + s*dxx[d];
+void kill() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (map[i][j] > 0) {
+				kill_map[i][j] += map[i][j];
+				for (int d = 0; d < 4; d++) {
+					for (int s = 1; s <= K; s++) {
+						int now_y = i + s * dyy[d];
+						int now_x = j + s * dxx[d];
 
-                        if(now_y >= 0 && now_y < n && now_x >= 0 && now_x < n){
-                            if(map[now_y][now_x] > 0){
-                                cnt += map[now_y][now_x];
-                            }else{
-                                break;
-                            }
-                        }else{
-                            break;
-                        }
-                    }
-                }
-                destroy_map[i][j] = cnt;
-            }
-        }
-    }
-    int max_cnt = -1;
-    int des_y = 0;
-    int des_x = 0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(map[i][j] > 0){
-                if(destroy_map[i][j] > max_cnt){
-                    max_cnt = destroy_map[i][j];
-                    des_y = i;
-                    des_x = j;
-                }else if(destroy_map[i][j] == max_cnt){
-                    if(i < des_y){
-                        des_y = i;
-                        des_x = j;
-                    }else if(i == des_y){
-                        if(j < des_x){
-                            des_y = i;
-                            des_x = j;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    if(max_cnt < 0){
-        flag = 1;
-        return;
-    }
-    sum += map[des_y][des_x];
-    map[des_y][des_x] = -c-1;
-    for(int d=0;d<4;d++){
-        for(int s=1;s<=k;s++){
-            int now_y = des_y + s*dyy[d];
-            int now_x = des_x + s*dxx[d];
+						if (now_y >= 0 && now_y < N && now_x >= 0 && now_x < N) {
+							if (map[now_y][now_x] > 0) {
+								kill_map[i][j] += map[now_y][now_x];
+							}
+							else {
+								break;
+							}
+						}
+						else {
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+	int max_n = -1;
+	int max_y = 0;
+	int max_x = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (kill_map[i][j] > max_n) {
+				max_n = kill_map[i][j];
+				max_y = i;
+				max_x = j;
+			}
+		}
+	}
+	result += max_n;
 
-            if(now_y >= 0 && now_y < n && now_x >= 0 && now_x < n){
-                if(map[now_y][now_x] > 0){
-                    sum += map[now_y][now_x];
-                    map[now_y][now_x] = -c-1;
-                }else{
-                    if(map[now_y][now_x] != -100){
-                        map[now_y][now_x] = -c-1;
-                    }
-                    break;
-                }
-            }else{
-                break;
-            }      
-        } 
-    }
-    memset(destroy_map,0,sizeof(destroy_map));
+	map[max_y][max_x] = 0;
+	death_map[max_y][max_x] = -C - 1;
+	for (int d = 0; d < 4; d++) {
+		for (int s = 1; s <= K; s++) {
+			int now_y = max_y + s * dyy[d];
+			int now_x = max_x + s * dxx[d];
+
+			if (now_y >= 0 && now_y < N && now_x >= 0 && now_x < N) {
+				if (map[now_y][now_x] > 0) {
+					map[now_y][now_x] = 0;
+					death_map[now_y][now_x] = -C - 1;
+				}
+				else {
+					death_map[now_y][now_x] = -C - 1;
+					break;
+				}
+			}
+			else {
+				break;
+			}
+		}
+	}
+	memset(kill_map, 0, sizeof(kill_map));
 }
 
-void heal_tree() {
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(map[i][j] < 0 && map[i][j] != -100){
-                map[i][j]++;
-            }
-        }
-    }
+void heal() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (death_map[i][j] < 0) {
+				death_map[i][j]++;
+			}
+		}
+	}
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int m;
-    cin >> n >> m >> k >> c;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin >> map[i][j];
-        }
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(map[i][j] == -1){
-                map[i][j] = -100;
-            }
-        }
-    }
-    sum = 0;
-    for(int t=0;t<m;t++){
-        flag = 0;
-        grow_tree();
-        spread_tree();
-        destroy_tree();
-        if(flag == 1){
-            break;
-        }
-        heal_tree();
-    }
-    cout << sum;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
 
-    return 0;
+	int M;
+	cin >> N >> M >> K >> C;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	result = 0;
+	for (int t = 1; t <= M; t++) {
+		grow();
+		breed();
+		kill();
+		heal();
+	}
+
+	cout << result;
+
+	return 0;
 }
